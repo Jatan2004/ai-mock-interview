5b676f7fba284503a2c263ba0b0d8f9701895a71
Jatan D <jatandarji365@gmail.com>
2026-02-08 17:11:20 +0530

    Size of resume upload increased

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,11 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
